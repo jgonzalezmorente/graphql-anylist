49c0a1c52d1418809c6fc90764c256e901f9b2be
José Antonio González Morente <jgonzalezmorente@gmail.com>
2023-12-18 11:33:21 +0100

    CRUD básico AnylistDB

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //* Elimina "silenciosamente" las propiedades que no estén en el DTO. Previene la contaminación de objetos con datos innecesarios o maliciosos
+      forbidNonWhitelisted: true  //* Dispara una excepción en caso de venir propiedades no definidas en el DTO. Actúa antes que el whitelist
+    })
+  );
+  await app.listen(3000);
+}
+
+
+
+bootstrap();
